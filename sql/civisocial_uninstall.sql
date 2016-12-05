53f9dcd499d863b715c484acb82466a07850555a
Edsel <edsel.lopez@jmaconsulting.biz>
2016-12-05 12:30:42 +0530

    MISC-119 Renamed SQL

--- sql/civisocial_uninstall.sql
+++
@@ -1,3 +0,0 @@
--- Drop tables
-DROP TABLE IF EXISTS `civicrm_civisocial_user`;
-DROP TABLE IF EXISTS `civicrm_civisocial_facebook_event`;
---
+++ sql/social_uninstall.sql
@@ -0,0 +1,3 @@
+-- Drop tables
+DROP TABLE IF EXISTS `civicrm_social_user`;
+DROP TABLE IF EXISTS `civicrm_social_facebook_event`;
